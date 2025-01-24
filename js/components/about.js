e203a3521ffd49d63581f8f71c53f6280bca53c3
NiklasG-127 <niklasgeldbach@gmail.com>
2025-01-24 14:15:42 +0100

    URLs

--- js/components/about.js
+++ js/components/about.js
@@ -44,7 +44,7 @@ export const About = {
             const token = sessionStorage.getItem('token');
 
             // API-Aufruf zur PostgreSQL-Datenbank über dein Backend
-            fetch('http://localhost:3000/api/data', {
+            fetch('https://testingbackendrepo.onrender.com/api/data', {
                 method: 'GET',
                 headers: {
                     'Authorization': `Bearer ${token}`, // Token im Authorization-Header senden
